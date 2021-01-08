5d2b71d23f2a16e8dc706cee46edd1a0af988a8e
kalaichelvan-kn <kalaichelvan294@gmail.com>
2021-01-08 08:03:33 +0530

    commit - 2

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Hello, world from GIT!");
+      // altered
+      // first commit
+      // second commit 
    }
 }
